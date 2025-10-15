d014c399abeaaba9c70800698469168640d3c790
Antonio-Sitoe <antoniositoehl@gmail.com>
2025-10-15 19:02:08 +0200

    ajustes

--- src/lib/email/config.ts
+++ src/lib/email/config.ts
@@ -1,19 +1,19 @@
-import nodemailer from 'nodemailer'
-import { env } from '@/lib/env'
+import nodemailer from 'nodemailer';
+import { env } from '@/lib/env';
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: env.EMAIL_USER,
-    pass: env.EMAIL_PASSWORD,
-  },
-  connectionTimeout: 60000,
-  greetingTimeout: 30000,
-  socketTimeout: 60000,
-  tls: {
-    rejectUnauthorized: false,
-  },
-})
+	service: 'gmail',
+	host: 'smtp.gmail.com',
+	port: 587,
+	secure: false,
+	auth: {
+		user: env.EMAIL_USER,
+		pass: env.EMAIL_PASSWORD,
+	},
+	connectionTimeout: 60000,
+	greetingTimeout: 30000,
+	socketTimeout: 60000,
+	tls: {
+		rejectUnauthorized: false,
+	},
+});
