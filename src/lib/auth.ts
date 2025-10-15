4cd43f5b0edccac88475ab5d367901779959bfc1
Antonio-Sitoe <antoniositoehl@gmail.com>
2025-10-15 20:46:52 +0200

    fix eslint and remove env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,71 +1,72 @@
-import { betterAuth } from 'better-auth';
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { bearer, emailOTP, openAPI } from 'better-auth/plugins';
-import { db } from '@/db/db';
-import { emailService } from './email/service';
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { bearer, emailOTP, openAPI } from 'better-auth/plugins'
+import { db } from '@/db/db'
+import { emailService } from './email/service'
 
 export const auth = betterAuth({
-	basePath: '/auth',
-	trustedOrigins: ['https://cbf4dbc024b2.ngrok-free.app'],
-	plugins: [
-		openAPI(),
-		bearer(),
-		emailOTP({
-			otpLength: 4,
-			async sendVerificationOTP({ email, otp, type }) {
-				await emailService.sendOTP({ email, otp, type });
-			},
-		}),
-	],
-	database: drizzleAdapter(db, {
-		provider: 'pg',
-		usePlural: true,
-	}),
-	advanced: {
-		database: {
-			generateId: false,
-		},
-	},
-	emailAndPassword: {
-		autoSignIn: true,
-		enabled: true,
-		password: {
-			hash: (password: string) => Bun.password.hash(password),
-			verify: ({ hash, password }) => Bun.password.verify(password, hash),
-		},
-	},
-	session: {
-		expiresIn: 60 * 60 * 24 * 7,
-		cookieCache: {
-			enabled: true,
-			maxAge: 60 * 5,
-		},
-	},
-});
+  basePath: '/auth',
+  trustedOrigins: ['https://cbf4dbc024b2.ngrok-free.app'],
+  plugins: [
+    openAPI(),
+    bearer(),
+    emailOTP({
+      otpLength: 4,
+      async sendVerificationOTP({ email, otp, type }) {
+        await emailService.sendOTP({ email, otp, type })
+        console.log('Sending verification OTP to', email, otp, type)
+      },
+    }),
+  ],
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    usePlural: true,
+  }),
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  emailAndPassword: {
+    autoSignIn: true,
+    enabled: true,
+    password: {
+      hash: (password: string) => Bun.password.hash(password),
+      verify: ({ hash, password }) => Bun.password.verify(password, hash),
+    },
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5,
+    },
+  },
+})
 
-let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>;
+let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>
 const getSchema = async () => {
-	_schema ??= auth.api.generateOpenAPISchema();
-	return _schema;
-};
+  _schema ??= auth.api.generateOpenAPISchema()
+  return _schema
+}
 
 export const OpenAPI = {
-	getPaths: (prefix = '/auth') =>
-		getSchema().then(({ paths }) => {
-			const reference: typeof paths = Object.create(null);
+  getPaths: (prefix = '/auth') =>
+    getSchema().then(({ paths }) => {
+      const reference: typeof paths = Object.create(null)
 
-			for (const path of Object.keys(paths)) {
-				const key = prefix + path;
-				reference[key] = paths[path];
+      for (const path of Object.keys(paths)) {
+        const key = prefix + path
+        reference[key] = paths[path]
 
-				for (const method of Object.keys(paths[path])) {
-					const operation = (reference[key] as any)[method];
+        for (const method of Object.keys(paths[path])) {
+          const operation = (reference[key] as any)[method]
 
-					operation.tags = ['Better Auth'];
-				}
-			}
+          operation.tags = ['Better Auth']
+        }
+      }
 
-			return reference;
-		}) as Promise<any>,
-	components: getSchema().then(({ components }) => components) as Promise<any>,
-} as const;
+      return reference
+    }) as Promise<any>,
+  components: getSchema().then(({ components }) => components) as Promise<any>,
+} as const
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,21 +1,20 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 const EnvSchema = z.object({
-	PORT: z.coerce.number().default(3000),
-	HOST: z.string().default('localhost'),
-	NODE_ENV: z.string().default('development'),
-	DATABASE_URL: z.string({ error: 'DATABASE_URL is required' }),
-	BETTER_AUTH_SECRET: z.string({ error: 'BETTER_AUTH_SECRET is required' }),
-	BETTER_AUTH_URL: z.string({ error: 'BETTER_AUTH_URL is required' }),
-	LOG_LEVEL: z.string({ error: 'LOG_LEVEL is required' }),
-	LOG_FILE: z.string({ error: 'LOG_FILE is required' }),
-	EMAIL_USER: z.string({ error: 'EMAIL_USER is required' }),
-	EMAIL_PASSWORD: z.string({ error: 'EMAIL_PASSWORD is required' }),
-	SMTP_HOST: z.string().optional(),
-	SMTP_PORT: z.string().optional(),
-	SMTP_SECURE: z.string().optional(),
-	SMTP_USER: z.string().optional(),
-	SMTP_PASS: z.string().optional(),
-});
+  PORT: z.coerce.number().default(3000),
+  HOST: z.string().default('localhost'),
+  NODE_ENV: z.string().default('development'),
+  DATABASE_URL: z.string({ error: 'DATABASE_URL is required' }),
+  BETTER_AUTH_SECRET: z.string({ error: 'BETTER_AUTH_SECRET is required' }),
+  LOG_LEVEL: z.string({ error: 'LOG_LEVEL is required' }),
+  LOG_FILE: z.string({ error: 'LOG_FILE is required' }),
+  EMAIL_USER: z.string({ error: 'EMAIL_USER is required' }),
+  EMAIL_PASSWORD: z.string({ error: 'EMAIL_PASSWORD is required' }),
+  SMTP_HOST: z.string().optional(),
+  SMTP_PORT: z.string().optional(),
+  SMTP_SECURE: z.string().optional(),
+  SMTP_USER: z.string().optional(),
+  SMTP_PASS: z.string().optional(),
+})
 
-export const env = EnvSchema.parse(process.env);
+export const env = EnvSchema.parse(process.env)
